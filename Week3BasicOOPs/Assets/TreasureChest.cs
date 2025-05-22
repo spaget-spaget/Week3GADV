de0d6ec507456373d001dc362d6fcd00efa59d1a
spaget-spaget <oliverlimyk@gmail.com>
2025-05-22 16:17:04 +0800

    OOP part 2
    more OOP stuff

--- Week3BasicOOPs/Assets/TreasureChest.cs
+++ Week3BasicOOPs/Assets/TreasureChest.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
 public class TreasureChest : MonoBehaviour
 {
+    
     public virtual void Unlock()
     {
         Debug.Log("You try to unlock the chest.");
     }
-    public virtual void Unlock(MasterKey)
+    public virtual void Unlock(bool MasterKey)
     {
-        Debug.Log("You dont have a MasterKey.");
+        if (MasterKey == true) 
+        {
+            Debug.Log("You dont have a MasterKey.")
+        }
+        else
+        {
+            Debug.Log("You unlock the chest with the master key.");
+        }   
     }
     public void Shake()
     {
@@ -20,27 +28,31 @@ public class TreasureChest : MonoBehaviour
 
 public class AncientChest : TreasureChest
 {
-    public override void Unlock()
-    {
-        public override void Unlock(MasterKey) 
+        public override void Unlock(bool MasterKey) 
+        {
+            Debug.Log("Bonus Money! You unlock the chest with a master key.");
+        }
+        public override void Unlock()
         {
             Debug.Log("You unlock the ancient chest with an ancient key.");
         }
-        Debug.Log("Bonus Money! You unlock the chest with a master key.");
-    }
-   
     
 }
 
 public class MagicChest : TreasureChest
 {
-    public override void Unlock()
-    {
-        public override void Unlock(MasterKey)
+    if(bool MasterKey == true){
+        public override void Unlock(bool MasterKey)
         {
             Debug.Log("Bonus Money! You unlock the chest with a master key.");
         }
-        Debug.Log("You cast a spell to unlock the magic chest.");
     }
-    
+    else
+    {
+        public override void Unlock()
+        {
+            Debug.Log("You unlock the Magic chest with magic.");
+        }
+    }
 }
+*/
